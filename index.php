a648b61c93f0c49eb7be3affbe018cc0ffb1a16d
SamHolbrook1608 <83363471+SamHolbrook1608@users.noreply.github.com>
2021-05-13 06:24:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
 
-include_once("main.js");
-include_once("head.ejs");
 include_once("index.ejs"); 
+include_once("head.ejs");
+include_once("main.js");
 include_once("package-lock.json");
 include_once("package.json"); 
 
